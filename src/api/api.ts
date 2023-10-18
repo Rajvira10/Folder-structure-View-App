c760a2171c5755d59c43952848a7451261f522cd
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-10-19 04:20:14 +0600

    minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 import { Folder } from "../types/Folder";
 
-const baseURL = import.meta.env.BASEURL || "http://localhost:3001/api/folders/"; // Replace with your API endpoint
+const baseURL = import.meta.env.BASEURL || "http://localhost:3001/api/folders/";
 
-// Fetch the folder structure
 export const getFolderStructure = async (): Promise<Folder[]> => {
   const response = await axios.get<Folder[]>(baseURL);
   return response.data;
 };
 
-// Create a new folder
 export const createFolder = async (
   name: string,
   parent?: string
@@ -18,7 +16,6 @@ export const createFolder = async (
   return response.data;
 };
 
-// Delete a folder
 export const deleteFolder = async (id: string): Promise<void> => {
   await axios.delete(`${baseURL}/${id}`);
 };
